8f1fc40ffd37878d3885365e540ad363502256a8
rayan <rayancooray123456@gmail.com>
2025-02-15 22:37:43 +0530

    hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -37,7 +37,7 @@ const Hero = () => {
               className="bg-gradient-to-r from-pink-300 via-slate-500  to-purple-500
               bg-clip-text text-4xl tracking-tight text-transparent"
             >
-            Back End Developer
+            Full Stack Developer
             </motion.span>
             <motion.p
               variants={container(1)}
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,16 +10,24 @@ const Navbar = () => {
   return (
     <nav className='mb-20 flex items-center justify-between py-6'>
       <div className='flex flex-shrink-0 items-center'>
-      <img className='mx-2 w-10'  src={logo} alt='logo'/>
+        <img className='mx-2 w-10' src={logo} alt='logo' />
       </div>
       <div className='m-8 flex items-center justify-center gap-4 text-2xl'>
-        <FaLinkedin href='https://www.linkedin.com/in/rayan-cooray-3193a1262/'/>
-        <FaGithub href='https://github.com/rayanCooray/'/>
-        <FaInstagram href='https://www.instagram.com/rayan_cooray/'/>
-        <FaSquareXTwitter href='https://twitter.com/rayan_cooray'/>
+        <a href='https://www.linkedin.com/in/rayan-cooray-3193a1262/' target='_blank' rel='noopener noreferrer'>
+          <FaLinkedin />
+        </a>
+        <a href='https://github.com/rayanCooray/' target='_blank' rel='noopener noreferrer'>
+          <FaGithub />
+        </a>
+        <a href='https://www.instagram.com/rayan_cooray/' target='_blank' rel='noopener noreferrer'>
+          <FaInstagram />
+        </a>
+        <a href='https://twitter.com/rayan_cooray' target='_blank' rel='noopener noreferrer'>
+          <FaSquareXTwitter />
+        </a>
       </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
